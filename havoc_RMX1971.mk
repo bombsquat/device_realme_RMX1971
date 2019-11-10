133dc5824f7ec49121e51f0f4dd6edb50743cc50
Warfraid <yashambekar97@gmail.com>
2019-11-10 13:34:39 +0530

    Update havoc_RMX1971.mk

--- havoc_RMX1971.mk
+++ havoc_RMX1971.mk
@@ -3,6 +3,10 @@
 #
 # SPDX-License-Identifer: Apache-2.0
 #
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
 
 # Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/havoc/config/common.mk)
